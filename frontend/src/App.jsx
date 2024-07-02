9f83f5cf2fc587fc3b8570f6927e5995f606e5b3
davibcavalcante <davib506@gmail.com>
2024-07-02 01:54:40 -0300

    Estrutura básica do projeto finalizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './styles/index.css';
 
+import { Outlet } from 'react-router-dom';
+
 function App() {
 
   return (
     <>
-      
+      <h1>Testando Reaproveitamento de Componentes com React Router</h1>
+      <Outlet />
+      <h2>Funcionando!</h2>
     </>
   );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <nav></nav>
+    );
+}
+
+export default Login;

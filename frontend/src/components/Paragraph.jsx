cadf7e96850aeacce68be293d8e63962e17437c3
martinsdev <martins.devv@gmail.com>
2024-07-10 16:06:43 -0300

    Login/Register API

--- frontend/src/components/Paragraph.jsx
+++
@@ -1,7 +0,0 @@
-const Paragraph = ({ children }) => {
-    return (
-        <p className="text-white text-xl font-poppins 2xl:text-2xl">{children}</p>
-    );
-}
-
-export default Paragraph;
--- frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-    return (
-        <h1 className="font-poppins">Home Page</h1>
-    );
-}
-
-export default Home;
